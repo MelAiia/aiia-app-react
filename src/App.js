383a0ff701f58693d23104972b8fb0e62d1cee97
MelAiia <shavlakayya@gmail.com>
2023-09-07 15:51:55 +0300

    Added new component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Weather from "./Weather";
+import Source from "./Source";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Source />
         <h1>Weather app by Aiia</h1>
         <Weather />
       </header>
---
+++ src/Source.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Source() {
+  return (
+    <small className="Source">
+      <a
+        href="https://github.com/MelAiia/aiia-app-react"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open-source code
+      </a>
+      , by Melnychenko Aiia
+    </small>
+  );
+}
